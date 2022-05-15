910f99689ca402653b932c013de7045b68466279
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-05-15 19:24:05 +0200

    Iteracion4

--- scripts.js
+++ scripts.js
@@ -64,3 +64,79 @@ for (let user of users) {
     result = sum / counter;
 };
 console.log(result);
+
+console.log('----------------');
+
+/* Iteracion 3 */
+
+const users2 = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+];
+
+let userSound;
+let soundCounter = 0;
+let soundsResult = [];
+for (let userName of users2) {
+    console.log(userName.favoritesSounds);
+    for (sound in userName.favoritesSounds) {
+        soundsResult.push(sound);
+    }
+}; 
+console.log(soundsResult);
+
+console.log('----------');
+
+/* Iteracion 4 */
+
+const animales = ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
+function findArrayIndex(array, text) {
+    let posicionAnimal = array.indexOf(text);
+    if (array.includes(text)) {
+        console.log(`El animal ${text} está en la posición ${posicionAnimal}`);
+    } else {
+        console.log(`No tenemos ese animal`);
+    }
+};
+
+findArrayIndex(animales, 'Mosquito');
+findArrayIndex(animales, 'perro');
+
+// Mejora: que acepte animales escribiendo todo en minúsculas
+
+
+
+
+
+
+
+
+
+
+
