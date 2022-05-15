875528479fde7d9a9c45d7e4ae34ba026b3ac767
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-05-15 13:29:37 +0200

    Iteracion2

--- scripts.js
+++ scripts.js
@@ -18,3 +18,49 @@ for (let movie of movies) {
 console.log(categoriesArr);
 
 console.log('----------------');
+
+/* Iteración 2 */
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+];
+
+let sum = 0;
+let result;
+let counter = 0;
+for (let user of users) {
+    for (sound in user.favoritesSounds) {
+        let volume = user.favoritesSounds[sound].volume;
+        counter ++;
+        sum += volume;
+    };
+    result = sum / counter;
+};
+console.log(result);
