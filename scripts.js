e79cfa4cc0399d1b96e945f5eb2f0679641554a2
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-05-15 19:32:49 +0200

    Iteracion5

--- scripts.js
+++ scripts.js
@@ -130,7 +130,17 @@ findArrayIndex(animales, 'perro');
 
 // Mejora: que acepte animales escribiendo todo en minúsculas
 
+console.log('----------');
 
+/* Iteración 5 */
+
+//Crea una función llamada rollDice() que reciba como parametro el numero de caras que queramos que tenga el dado que deberá silumar el codigo dentro de la función. Como hemos dicho, que la función use el parametro para simular una tirada de dado y retornar el resultado. Si no se te ocurre como hacer un numero aleatorio no te preocupes! busca información sobre la función de javascript Math.random();
+
+function rollDice(sidesNumber) {
+    return Math.floor(Math.random() * (sidesNumber - 1) + 1);
+};
+
+console.log(rollDice(6));
 
 
 
